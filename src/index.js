76bab7fc6cf9824acbb66b909d55bea942ca1761
johanvx <johanpoisson@outlook.com>
2022-07-02 17:51:09 +0800

    style(theme): use a single file to configure the theme

--- src/index.js
+++ src/index.js
@@ -1,33 +1,18 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-
-const theme = extendTheme({
-  colors: {
-    brand: {
-      light: "#0052cc",
-      dark: "#0047b3",
-    },
-  },
-});
+import theme from "./lib/theme";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StrictMode>
     <ChakraProvider theme={theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </ChakraProvider>
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
