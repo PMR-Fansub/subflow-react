52165f5d345335fd6f91581792b3c0a9fb082b20
Johan Xie <johanpoisson@outlook.com>
2023-07-26 00:26:40 +0800

    refactor: create a new component to reduce code duplication

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -23,7 +23,7 @@ import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import { Link as RouterLink, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useAuth } from "../utils/auth";
-import { useForm } from "react-hook-form";
+import { FieldValues, RegisterOptions, useForm } from "react-hook-form";
 import SubFlowLogoFull from "../assets/subflow-full.svg";
 import SubFlowLogoFullWhite from "../assets/subflow-full-white.svg";
 import { AxiosError } from "axios";
@@ -43,14 +43,7 @@ const Register = () => {
 
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  const handleRegister = (values: {
-    username: string;
-    email: string;
-    password: string;
-    repeatPassword: string;
-  }) => {
-    console.log(values);
-
+  const handleRegister = (values: FieldValues) => {
     auth
       .register(values.username, values.email, values.password)
       .then(() => {
@@ -79,6 +72,60 @@ const Register = () => {
         });
       });
   };
+
+  const FormItem = ({
+    name,
+    label,
+    option,
+    inputType,
+    autoComplete,
+    inputRightElement
+  }: {
+    name: string;
+    label: string;
+    option: RegisterOptions;
+    autoComplete?: string;
+    inputType?: React.HTMLInputTypeAttribute;
+    inputRightElement?: React.ReactNode;
+  }) => {
+    const CustomInput = () => (
+      <Input
+        id={name}
+        type={inputType ?? "text"}
+        autoComplete={autoComplete ?? name}
+        {...register(name, option)}
+      />
+    );
+
+    const CustomInputGroup = () =>
+      inputRightElement ? (
+        <InputGroup>
+          <CustomInput />
+          <InputRightElement h="full">{inputRightElement}</InputRightElement>
+        </InputGroup>
+      ) : (
+        <CustomInput />
+      );
+
+    return (
+      <FormControl id={name} isRequired isInvalid={!!errors[name]}>
+        <FormLabel htmlFor={name}>{label}</FormLabel>
+        <CustomInputGroup />
+        <FormErrorMessage>
+          {errors[name] && errors[name]?.message?.toString()}
+        </FormErrorMessage>
+      </FormControl>
+    );
+  };
+
+  const ToggleShowPasswordButton = () => (
+    <Button
+      variant={"ghost"}
+      onClick={() => setShowPassword(showPassword => !showPassword)}
+    >
+      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+    </Button>
+  );
 
   return (
     <Flex align={"center"} justify={"center"}>
@@ -109,98 +156,49 @@ const Register = () => {
         >
           <form onSubmit={handleSubmit(handleRegister)}>
             <Stack spacing={4}>
-              <FormControl id="username" isRequired isInvalid={errors.username}>
-                <FormLabel htmlFor="username">用户名</FormLabel>
-                <Input
-                  id="username"
-                  autoComplete="username"
-                  {...register("username", {
-                    required: "用户名不可为空",
-                    minLength: { value: 4, message: "用户名长度不可小于4" },
-                    maxLength: { value: 32, message: "用户名长度不可大于32" }
-                  })}
-                />
-                <FormErrorMessage>
-                  {errors.username && errors.username.message}
-                </FormErrorMessage>
-              </FormControl>
-              <FormControl id="email" isRequired isInvalid={errors.email}>
-                <FormLabel htmlFor="email">邮箱地址</FormLabel>
-                <Input
-                  id="email"
-                  type="email"
-                  autoComplete="email"
-                  {...register("email", {
-                    required: "邮箱地址不可为空",
-                    pattern: {
-                      value: emailRegex,
-                      message: "邮箱地址格式不正确"
-                    }
-                  })}
-                />
-                <FormErrorMessage>
-                  {errors.email && errors.email.message}
-                </FormErrorMessage>
-              </FormControl>
-              <FormControl id="password" isRequired isInvalid={errors.password}>
-                <FormLabel htmlFor="password">密码</FormLabel>
-                <InputGroup>
-                  <Input
-                    id="password"
-                    type={showPassword ? "text" : "password"}
-                    autoComplete="new-password"
-                    {...register("password", {
-                      required: "密码不可为空",
-                      minLength: { value: 6, message: "密码长度不可小于6" },
-                      maxLength: { value: 64, message: "密码长度不可大于64" }
-                    })}
-                  />
-                  <InputRightElement h={"full"}>
-                    <Button
-                      variant={"ghost"}
-                      onClick={() =>
-                        setShowPassword(showPassword => !showPassword)
-                      }
-                    >
-                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-                <FormErrorMessage>
-                  {errors.password && errors.password.message}
-                </FormErrorMessage>
-              </FormControl>
-              <FormControl
-                id="repeatPassword"
-                isRequired
-                isInvalid={errors.repeatPassword}
-              >
-                <FormLabel htmlFor="repeatPassword">重复密码</FormLabel>
-                <InputGroup>
-                  <Input
-                    id="repeatPassword"
-                    type={showPassword ? "text" : "password"}
-                    autoComplete="new-password"
-                    {...register("repeatPassword", {
-                      validate: value =>
-                        value === watch("password") || "两次密码不一致"
-                    })}
-                  />
-                  <InputRightElement h={"full"}>
-                    <Button
-                      variant={"ghost"}
-                      onClick={() =>
-                        setShowPassword(showPassword => !showPassword)
-                      }
-                    >
-                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-                <FormErrorMessage>
-                  {errors.repeatPassword && errors.repeatPassword.message}
-                </FormErrorMessage>
-              </FormControl>
+              <FormItem
+                name="username"
+                label="用户名"
+                option={{
+                  required: "用户名不可为空",
+                  minLength: { value: 4, message: "用户名长度不可小于4" },
+                  maxLength: { value: 32, message: "用户名长度不可大于32" }
+                }}
+              />
+              <FormItem
+                name="email"
+                label="邮箱地址"
+                option={{
+                  required: "邮箱地址不可为空",
+                  pattern: {
+                    value: emailRegex,
+                    message: "邮箱地址格式不正确"
+                  }
+                }}
+              />
+              <FormItem
+                name="password"
+                label="密码"
+                inputType={showPassword ? "text" : "password"}
+                autoComplete="new-password"
+                option={{
+                  required: "密码不可为空",
+                  minLength: { value: 6, message: "密码长度不可小于6" },
+                  maxLength: { value: 64, message: "密码长度不可大于64" }
+                }}
+                inputRightElement={<ToggleShowPasswordButton />}
+              />
+              <FormItem
+                name="repeatPassword"
+                label="重复密码"
+                inputType={showPassword ? "text" : "password"}
+                option={{
+                  validate: value =>
+                    value === watch("password") || "两次密码不一致"
+                }}
+                autoComplete="new-password"
+                inputRightElement={<ToggleShowPasswordButton />}
+              />
               <Stack spacing={10} pt={2}>
                 <Button
                   loadingText="Submitting"
