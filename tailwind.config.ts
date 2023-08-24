94210e74c7fc1d1d1241eb931500f8cbd73cc6ec
Johan Xie <johanpoisson@outlook.com>
2023-08-25 03:36:26 +0800

    chore: use TypeScript configuration file for TailwindCSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {}
   },
   plugins: []
-};
+} satisfies Config;
